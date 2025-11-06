6030ad8575311bb03e84b4554caa519141457c6c
beaualbritton <albrittoncb@gmail.com>
2025-11-06 16:53:10 -0500

    fix/feat: proper commit info for objects from branches
    instead of just iterating over git_repo.head.target (HEAD) which are commits under the checked out branch (master, in this case) search commits in all branches recursively, most importantly, checking which commits have already been visited in  (set) to reduce overall search time

--- django/backend/api/lib/git/commits.py
+++ django/backend/api/lib/git/commits.py
@@ -1,6 +1,7 @@
 from pygit2 import Repository, Commit, GIT_SORT_TIME
 from rest_framework.response import Response
 
+#TODO: update to fetch all commits 
 
 # find all commits in a repo
 def all_commits(git_repo) -> Response:
@@ -19,30 +20,52 @@ def all_commits(git_repo) -> Response:
 # given an object id, walk the tree and find commits that contain object
 def find_commit_refs(git_repo, git_object):
     commit_refs = []
-    for commit in git_repo.walk(git_repo.head.target, GIT_SORT_TIME):
-        current_commit = commit.peel(Commit)
-        if contains_object(git_repo, current_commit.tree, git_object.id):
-            commit_refs.append({
-                "oid": str(current_commit.id),
-                "author": current_commit.author.name,
-                "message": current_commit.message,
-                "time": current_commit.commit_time})
+    visited_commits = set()
 
+    for branches in git_repo.branches.local:
+        branch = git_repo.branches[branches]
+        branch_oid = branch.target
+
+        for commit in git_repo.walk(branch_oid, GIT_SORT_TIME):
+            # if commit has already been visited -- skip, some commits are shared
+            if commit in visited_commits:
+                continue
+            visited_commits.add(commit.id)
+            current_commit = commit.peel(Commit)
+
+            if contains_object(git_repo, current_commit.tree, git_object.id):
+                commit_refs.append({
+                    "oid": str(current_commit.id),
+                    "author": current_commit.author.name,
+                    "message": current_commit.message,
+                    "time": current_commit.commit_time
+                })
     return Response({"status": True, "commits": commit_refs})
 
 
 # given an object id, find the first commit that contains object
 def find_latest_ref(git_repo, git_object) -> Response:
     latest_ref = None
-    for commit in git_repo.walk(git_repo.head.target, GIT_SORT_TIME):
-        current_commit = commit.peel(Commit)
-        if contains_object(git_repo, current_commit.tree, git_object.id):
-            latest_ref = {
-                "oid": str(current_commit.id),
-                "author": current_commit.author.name,
-                "message": current_commit.message,
-                "time": current_commit.commit_time}
-            break
+    visited_commits = set()
+
+    for branches in git_repo.branches.local:
+        branch = git_repo.branches[branches]
+        branch_oid = branch.target
+
+        for commit in git_repo.walk(branch_oid, GIT_SORT_TIME):
+            # if commit has already been visited -- skip, some commits are shared
+            if commit in visited_commits:
+                continue
+            visited_commits.add(commit.id)
+            current_commit = commit.peel(Commit)
+
+            if contains_object(git_repo, current_commit.tree, git_object.id):
+                latest_ref = {
+                    "oid": str(current_commit.id),
+                    "author": current_commit.author.name,
+                    "message": current_commit.message,
+                    "time": current_commit.commit_time}
+                break
     return Response({"status": True, "commits": latest_ref})
 
 
