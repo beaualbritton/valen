6e03a3348a74740f58282529c763c0c282da4b8a
beau albritton <albrittoncb@gmail.com>
2025-11-11 17:37:13 -0500

    feat: very basic token authentication for django service
    TODO: configure nginx to make a request to django service

--- django/backend/api/views/auth.py
+++ django/backend/api/views/auth.py
@@ -1,11 +1,13 @@
+import base64
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, make_password, check_password
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
 from api.models import Profile
 from api.lib.server.directory import create_user_dir
+from api.models import Token
 # aliases
 create_user = User.objects.create_user
 
@@ -69,3 +71,32 @@ def logout_user(request):
 @api_view(["GET"])
 def csrf_token(request):
     return JsonResponse({"csrfToken": get_token(request)})
+
+
+@api_view(["GET"])
+def git_authentication(request):
+    # git sends tokens thru http -> Authorization: Basic <base64hash>
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Authorization
+    header = request.headers.get('Authorization')
+    if not header:
+        return Response(status=401)
+
+    # find the base64 hash in the header, decode it and split
+    b64_decode = base64.b64decode(header[6:]).decode()
+    b64_decode = b64_decode.split(':', 1)
+
+    username = b64_decode[0]
+    token = b64_decode[1]
+    hash = make_password(token)
+
+    user = User.objects.get(username=username)
+    if not user:
+        return Response(status=401)
+    user_tokens = Token.objects.filter(user=user)
+
+    for token in user_tokens:
+        if check_password(hash, token.hash):
+            return Response(status=200)
+
+    # no tokens or headers are incorrect
+    return Response(status=401)
