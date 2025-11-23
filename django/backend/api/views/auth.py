aab6ab5c12a5ac0ae6822b99c8ff4734093f0c0a
beau albritton <albrittoncb@gmail.com>
2025-11-23 16:31:16 -0500

    feat: https authentication for repo ownership & privacy.
    faster hashing with sha256 instead of make_password. tokens are already high entropy so sha256 is more than reasonable for encryption. caching DB queries as git makes multiple requests for reads/writes, measured latency @ around .4s locally. extended connection time, added index to relevant model fields

--- django/backend/api/views/auth.py
+++ django/backend/api/views/auth.py
@@ -1,6 +1,7 @@
-import base64
+import base64, hashlib
 from api.views import repository
 from rest_framework.decorators import api_view
+from django.core.cache import cache
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
@@ -12,7 +13,7 @@ from django.middleware.csrf import get_token
 from api.serializers import UserSerializer
 from api.models import Profile
 from api.lib.server.directory import create_user_dir
-from api.lib.git.resolve import resolve_repo_path
+from api.lib.git.resolve import resolve_repo_http, resolve_repo_ssh
 from api.models import Token, Repository
 from pathlib import Path
 
@@ -91,36 +92,71 @@ def csrf_token(request):
 def git_authentication(request):
     # git sends tokens thru http -> Authorization: Basic <base64hash>
     uri = request.META.get("HTTP_X_ORIGINAL_URI", "")
-
     # return 401 with authenticate header by default
     authenticate_res = HttpResponse(status=401)
     authenticate_res["WWW-Authenticate"] = 'Basic realm="Git"'
 
-    if "git-upload-pack" in uri:
-        return HttpResponse(status=200)
+    auth = request.META.get("HTTP_AUTHORIZATION", "")
 
-    if "git-receive-pack" in uri:
+    repo_name = resolve_repo_http(uri)
+
+    repo_key = f"repo_key:{repo_name}"
+    repo = cache.get(repo_key)
+    if repo is None:
+        repo = Repository.objects.get(repo_name=repo_name)
+        cache.set(repo_key, repo, 30)
+
+
+    user = None
+    is_valid = False
+    if auth.startswith("Basic "):
         try:
-            auth = request.META.get("HTTP_AUTHORIZATION", "")
-
-            if not auth.startswith("Basic "):
-                return authenticate_res
-
             b64_decode = base64.b64decode(auth[6:]).decode()
             b64_decode = b64_decode.split(":", 1)
-
             username = b64_decode[0]
             auth_token = b64_decode[1]
 
-            user = User.objects.get(username=username)
-            user_tokens = Token.objects.filter(user=user)
+            auth_token_hash = hashlib.sha256(auth_token.encode()).hexdigest()
 
-            for token in user_tokens:
-                if check_password(auth_token, token.hash):
-                    return HttpResponse(status=200)
+            auth_cache_key = f"auth:{user}:{auth_token_hash[:16]}"
+            cached_result = cache.get(auth_cache_key)
 
+            if cached_result:
+                user, is_valid = cached_result
+                
+            else:
+                user = User.objects.get(username=username)
+                is_valid = Token.objects.filter(user=user, hash=auth_token_hash).exists()
+                cache.set(auth_cache_key, (user, is_valid), 30)
+
+            if not is_valid:
+                return authenticate_res
         except Exception:
             return authenticate_res
+
+    
+    if "git-upload-pack" in uri:
+        if repo.public:
+            print("public")
+            return HttpResponse(status=200)
+
+        #TODO: collaborators
+        elif user and (user == repo.owner):
+            print("private but owner")
+
+            if is_valid:
+                return HttpResponse(status=200)
+
+        else:
+            print("private")
+
+    if "git-receive-pack" in uri:
+        #TODO: collaborators
+        if user and (user == repo.owner):
+            print("owner push")
+
+            if is_valid:
+                return HttpResponse(status=200)
 
     return authenticate_res
 
@@ -140,8 +176,7 @@ def ssh_validation(request):
     user = request.data.get("username")
     command = request.data.get("command")
 
-    # TODO: resolve repo name from command path
-    repo_name = resolve_repo_path(command)
+    repo_name = resolve_repo_ssh(command)
 
     repo = Repository.objects.get(repo_name=repo_name)
     
@@ -151,7 +186,6 @@ def ssh_validation(request):
     print(repo.repo_name)
 
     if "git-upload-pack" in command:
-        # TODO: check if public or private 
         print("clone")
         if repo.public:
             print("public")
@@ -164,7 +198,6 @@ def ssh_validation(request):
             print("private")
 
     elif "git-receive-pack" in command:
-        # TODO: push only allowed if user is owner or collaborator
         print("push")
         # TODO: collaborators
         if (user == repo.owner):
