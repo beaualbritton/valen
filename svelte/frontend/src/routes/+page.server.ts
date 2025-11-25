34806d7400a8f80fb907f87fd4d31ced1ab7022a
beau albritton <albrittoncb@gmail.com>
2025-11-24 23:28:44 -0500

    feat: basic home layout w/ landing page vs. dashboard depending on auth status

---
+++ svelte/frontend/src/routes/+page.server.ts
@@ -0,0 +1,20 @@
+import type {PageServerLoad} from './$types'
+import { getUser } from '$lib/api/login';
+
+export const load : PageServerLoad = async ({cookies, fetch}) => 
+{
+  const csrfToken = cookies.get('csrftoken');
+  const sessionId = cookies.get('sessionid');
+
+
+  const userResponse = await getUser({csrfToken, sessionId});
+  let status: boolean = userResponse.user.status
+  let user = null;
+
+  if(status)
+  {
+    user = userResponse.user.user.username;
+  }
+  //TODO: Consider returning in a more elegant way
+  return { user: user, status: status};
+}
